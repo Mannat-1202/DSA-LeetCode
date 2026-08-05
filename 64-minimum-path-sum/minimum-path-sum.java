13a754c13f52dc4d1086056dfcfcb850b2573155
Mannat-1202 <mannatsingh787@gmail.com>
2026-08-05 11:16:05 +0530

    Time: 4 ms (62.42%) | Memory: 50 MB (91.99%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+        int [][] dp = new int[n][m];
+        dp[0][0] = grid[0][0];
+        //first row
+        for(int i=1;i<m;i++){
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+        //first col
+        for(int j=1;j<n;j++){
+            dp[j][0] = grid[j][0] + dp[j-1][0];
+        }
+        //remaining cell
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        return dp[n-1][m-1];
+        
+    }
+}
