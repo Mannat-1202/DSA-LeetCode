1af1601eee7271daf55716147ba9feaac0c8284d
Mannat-1202 <mannatsingh787@gmail.com>
2026-08-08 07:54:44 +0530

    Time: 1 ms (91.74%) | Memory: 45 MB (53.30%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,33 +1,30 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-
+        
         int n = edges.length;
         int[] parent = new int[n+1];
-
         for(int i=1;i<=n;i++){
             parent[i] = i;
         }
         for(int[] edge : edges){
-
             int u = edge[0];
             int v = edge[1];
-
-            int root1 = find(u,parent);
-            int root2 = find(v,parent);
-
-            if(root1 == root2){
+            int rootu = find(u,parent);
+            int rootv = find(v,parent);
+            if(rootu == rootv){
                 return edge;
             }
-            parent[root1] = root2;
+            parent[rootu] = rootv;
         }
-       return new int[0];
+        return new int[0];
     }
     public int find(int node,int[] parent){
-
         if(parent[node] == node){
             return node;
         }
         parent[node] = find(parent[node],parent);
         return parent[node];
     }
+
+
 }
